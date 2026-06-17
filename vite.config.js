9e3223909560ef061e27196865d8255c310ed512
Madni112 <madnimemon022@gmail.com>
2026-06-17 18:30:39 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: { _global: {} },
-  server:{
-    port:2500
+  define: {
+    _global: "globalThis" 
+  },
+  server: {
+    port: 2500
   }
 });
